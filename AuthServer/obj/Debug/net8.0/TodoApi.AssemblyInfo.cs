c89528b53d4478196252ba8fa7b2d3723774bdd5
feigy1 <feigy23517@gmail.com>
2025-02-14 01:49:12 +0200

    update jwt

--- AuthServer/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ AuthServer/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04340a5a995c88ac98c8bff41922c6b85a9cf6fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4d135edcd067f2040c8d4ca6356561a389b1f31")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
